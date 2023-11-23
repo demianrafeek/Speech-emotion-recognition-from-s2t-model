ce1d9a8c55ac2f20e35c80a6589e47de7e2c94bd
Demian Rafeek <82714620+demianrafeek@users.noreply.github.com>
2023-11-23 15:26:21 +0200

    finetuning_s2t.py

--- finetuning_s2t.py
+++ finetuning_s2t.py
@@ -8,25 +8,24 @@ output_dir = "G:\Arete\projects\SER_project\\finetune_s2t_model\\output"
 # however, the token set is only needed for non-finetuned models
 # if you pass a new token set for an already finetuned model, it'll be ignored during training
 
-tokens = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]#, "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "'"]
-# tokens = ["ا", "ب", "ت", "ث", "ج", "ح", "خ", "د", "ذ","ر", "ز", "س", "ش", "ص", "ض", "ط", "ظ", "ع", "غ", "ف", "ق", "ك", "ل", "م", "ن", "ه", "و", "ي", "'"]
+# # finetune the model with the iemocap dataset 
+tokens = ['n', 'f', 'a', 's', 'h', 'e', 'z', 'r', 'o', 'd']
 token_set = TokenSet(tokens)
 
-# define your custom train data
-train_data = [
-    {"path": "G:\Arete\projects\SER_project\\finetune_s2t_model\\en_test1.wav", "transcription": "say the word bar"},
-    {"path": "G:\Arete\projects\SER_project\\finetune_s2t_model\\en_test2.wav", "transcription": "say the word date"},
-]
-
-# train_data = [
-#     {"path": "G:\Arete\projects\SER_project\\finetune_s2t_model\\ar_test1.wav", "transcription": " لا تفكر في ابسط ما يمكنك فعله بل فكر في اكبر ما يمكنك فعله"},
-#     {"path": "G:\Arete\projects\SER_project\\finetune_s2t_model\\ar_test2.wav", "transcription": "اننا نتحدث بنفس الوتيره اللي نفكر ونفهم بها الامور"},
-# ]
+# # define your custom train data
+# data = pd.read_csv('/media/ires/DATA/Speech_task RA/archive/Speech-emotion-recognition-from-s2t-model/iemocap_short_waves_data.csv')
+# path = '/media/ires/DATA/Speech_task RA/archive/Iemocap_audio/iemocap_audio/IEMOCAP_wav/'
+data = pd.read_csv('/iemocap_40s_data_with_dublicats.csv')
+path = '/media/ires/DATA/Speech_task RA/archive/Speech-emotion-recognition-from-s2t-model/iemocap_40s_waves/'
+data = [{'path':path+name, 'transcription':script} for name,script in zip(data['name'],data['emo_script'])]
+train_data = data[:int(len(data)*0.8)]
+eval_data  = data[int(len(data)*0.8):int(len(data)*0.9)]
 
 # and finally, fine-tune your model
 model.finetune(
     output_dir, 
     train_data=train_data,
+    eval_data=eval_data
     token_set=token_set,
 )
 
