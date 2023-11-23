46c2d2be8335e4c8c4e2acb81062baa8bf78e1b0
Demian Rafeek <82714620+demianrafeek@users.noreply.github.com>
2023-11-23 15:32:25 +0200

    Update model_test.py

--- model_test.py
+++ model_test.py
@@ -4,13 +4,13 @@ import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = SpeechRecognitionModel("output/", device=device)
 
-data = pd.read_csv(path+'iemocap_40s_data.csv')
+data = pd.read_csv(path+'/iemocap_40s_data_with_dublicats.csv')
 data_path = path+'iemocap_40s_waves/'
 data = data.iloc[int(len(data)*0.9):,:]
 
 test_data = [data_path+name for name in data['name']]
 
-transcriptions = model.transcribe(audio_paths)
+transcriptions = model.transcribe(test_data)
 print(transcriptions)
 
 
